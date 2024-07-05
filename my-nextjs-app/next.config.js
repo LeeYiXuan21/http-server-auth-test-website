37176ab80f187174e0ded418cccc5cc08d29116c
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-05 17:36:07 +0800

    remove pages

--- my-nextjs-app/next.config.js
+++ my-nextjs-app/next.config.js
@@ -1,19 +1,6 @@
 module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
-    exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      return {
-        '/': { page: '/' },
-        // Add paths for all your static pages
-        '/basicauth': { page: '/basicauth' },
-        '/basicauth1': { page: '/basicauth1' },
-        '/basicauth2': { page: '/basicauth2' },
-        // Repeat for all 100 pages
-      };
-    },
     // Configure basePath if your repository name is different from the project name
     basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-app' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/my-nextjs-app/' : ''
