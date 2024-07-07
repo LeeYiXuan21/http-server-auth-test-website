c4ad6512c4815779d039222f4887f31cce37064e
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-07 23:19:13 +0800

    remove nextjs

--- my-nextjs-app/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    reactStrictMode: true,
-    trailingSlash: true,
-    // Configure basePath if your repository name is different from the project name
-    basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-app' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-nextjs-app/' : ''
-  };
