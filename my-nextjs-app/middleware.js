621e80c17f15f26019bfa770caeebf0f5c659872
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-07 23:17:26 +0800

    edit  error

--- my-nextjs-app/middleware.js
+++ my-nextjs-app/middleware.js
@@ -6,18 +6,18 @@ const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
 
 export function basicAuthMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
-  
+
   if (authHeader) {
     const auth = authHeader.split(' ')[1];
     const credentials = base64.decode(auth).split(':');
     const username = credentials[0];
     const password = credentials[1];
-    
+
     if (username === USERNAME && password === PASSWORD) {
       return next();
     }
   }
-  
+
   res.setHeader('WWW-Authenticate', 'Basic realm="Access to the site"');
   res.status(401).end('Authentication required');
 }
