efcb79997f93923651debce2cda407baf619344d
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-08 09:52:58 +0800

    test

--- my-nextjs-app/next.config.js
+++ my-nextjs-app/next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   basePath: '/leeyixuan21.github.io', 
   assetPrefix: '/leeyixuan21.github.io/', 
   trailingSlash: true,
-  output: 'export',
-  async redirects() {
-    return [
-      {
-        source: '/basicauth',
-        destination: '/basicauth.html',
-        permanent: true,
-      },
-    ];
-  },
+  output: 'export'
 };
