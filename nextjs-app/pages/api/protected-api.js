fcc81be64140e183e2f45ff12a3b96efb904746d
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-09 14:24:27 +0800

    remove nextjs

--- nextjs-app/pages/api/protected-api.js
+++
@@ -1,8 +0,0 @@
-// pages/api/protected-api.js
-import middleware from '../../middleware';
-
-console.log('HTTP_BASIC_AUTH:', process.env.HTTP_BASIC_AUTH); // Check if environment variable is accessible
-
-export default function handler(req, res) {
-    return middleware(req, res);
-}
